b0c99088a16e3608e1505780eba72322a92c4153
jordles113 <jordan.t.romero@gmail.com>
2021-03-24 17:09:15 -0500

    creates Producer properties

--- src/main/java/com/github/jordles113/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/jordles113/kafka/tutorial1/ProducerDemo.java
@@ -1,7 +1,20 @@
 package com.github.jordles113.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //create Producer properties
+        String bootstrapServers = ("127.0.0.1:9092");
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // create the producer
+
+        // send data
     }
 }
